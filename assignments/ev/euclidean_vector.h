2b20426dff5d4cf107fd6f6ba00aa96a739a3672
us579 <wzus579@gmail.com>
2019-07-16 18:23:27 +1000

    add start code

---
+++ assignments/ev/euclidean_vector.h
@@ -0,0 +1,24 @@
+// TODO(you): Include header guards
+
+#include <exception>
+#include <memory>
+#include <string>
+
+class EuclideanVectorError : public std::exception {
+ public:
+  explicit EuclideanVectorError(const std::string& what) : what_(what) {}
+  const char* what() const noexcept { return what_.c_str(); }
+
+ private:
+  std::string what_;
+};
+
+class EuclideanVector {
+ public:
+  explicit EuclideanVector(int i);
+  friend std::ostream& operator<<(std::ostream& os, const EuclideanVector& v);
+  // TODO(you): add more
+ private:
+  std::unique_ptr<double[]> magnitudes_;
+  // TODO(you): add more
+};
