2591ba8afa464adfd2ebb6d63d4f71261ff10b4e
us579 <wzus579@gmail.com>
2019-07-19 17:26:28 +1000

    add sub class

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -10,18 +10,28 @@ template<typename N, typename E>
 class Graph {
  public:
   Graph()= default;
+  class Node;
+  class Edge;
 //  Graph<N, E>(std::vector<N> a,std::vector<N> b);
 
   class Node{
   public:
-    Node(const N& v):val{std::make_shared<N>(v)}{}
+    Node(const N& v):val_{std::make_shared<N>(v)}{}
     const N& getval(){return *this->val;}
   private:
-    std::shared_ptr<N> val;
+    std::shared_ptr<N> val_;
+    std::vector<std::shared_ptr<Edge>> outEdge_;
+    std::vector<std::shared_ptr<Edge>> inEdge_;
+
   };
   class Edge{
   public:
+    Edge(std::shared_ptr<Node> src,std::shared_ptr<Node> dst , const E& w):
+    begin_{src},end_{dst},weight_{w} {}
   private:
+    E weight_;
+    std::weak_ptr<Node> begin_;
+    std::weak_ptr<Node> end_;
   };
 
  private:
