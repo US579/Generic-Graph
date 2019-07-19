1b5fce21d908fe1aa6a8e5bf83f3c4b329ff38ba
Zhenhao Chen <z5141527@unsw.edu.au>
2019-07-19 12:28:38 +1000

    ass2--finish first 2 constructor

--- assignments/dg/client.cpp
+++ assignments/dg/client.cpp
@@ -3,5 +3,7 @@
 #include "assignments/dg/graph.h"
 
 int main() {
-  gdwg::Graph<std::string, int> g1;
+  // gdwg::Graph<std::string, int> g1;
+  std::vector<std::string> v{"Hello", "how", "are", "you"};
+  gdwg::Graph<std::string, double> b{v.begin(), v.end()};
 }
--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -1,18 +1,41 @@
 #ifndef ASSIGNMENTS_DG_GRAPH_H_
 #define ASSIGNMENTS_DG_GRAPH_H_
+#include <iterator>
+#include <vector>
 
 namespace gdwg {
 
-template<typename N, typename E>
+template <typename N, typename E>
 class Graph {
  public:
+  class Node {
+   public:
+    Node(N n) : node_{std::make_shared<N>(n)} {}
+
+   private:
+    std::shared_ptr<N> node_;
+  };
+  class Edge {
+   public:
+   private:
+    Node src;
+    Node dst;
+    std::shared_ptr<E> weight;
+  };
+  Graph<N, E>() = default;
+  Graph<N, E>(typename std::vector<N>::const_iterator a, typename std::vector<N>::const_iterator b);
+
  private:
+  std::vector<Node> nList;
+  std::vector<Edge> eList;
+  // std::vector<std::shared_ptr<N>> nList;
+  // std::vector<std::shared_ptr<std::tuple<N, N, E>>> eList;
 };
 
 // Define your graph_iterator here
 // Define your graph_const_iterator here
 
-}
+}  // namespace gdwg
 
 #include "assignments/dg/graph.tpp"
 
