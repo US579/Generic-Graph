8e5a0596ac7b9559e6e4b9ba4bbe2153fd664359
Zhenhao Chen <z5141527@unsw.edu.au>
2019-07-23 22:23:31 +1000

    reconstruct the node and edge to graph

--- assignments/dg/client.cpp
+++ assignments/dg/client.cpp
@@ -5,53 +5,76 @@
 #include "assignments/dg/graph.h"
 
 int main() {
-  gdwg::Graph<std::string, int> g1;
-  gdwg::Graph<int, int> g;
+  // gdwg::Graph<std::string, int> g1;
+  // gdwg::Graph<int, int> g;
 
   // test for vector iterator constructor
   std::vector<std::string> v{"Hello", "how", "are", "you"};
   gdwg::Graph<std::string, double> b{v.begin(), v.end()};
+  b.printG();
+  // auto lis2 = b.GetNodes();
+  // std::cout << "graph b2 is: \n";
+  // for (std::map<N, std::shared_ptr<Node>>::iterator it = mymap.begin();
+  //      it != mymap.end(); ++it)
+  //   std::cout << it->first << " => " << it->second << '\n';
+  // for (int i = 0; i < lis2.size(); ++i) {
+  //   std::cout << lis2[i] + " ";
+  // }
+  // std::cout << "\n";
 
-  // // test for tuple iterator constructor
-  std::string s1{"Hello"};
-  std::string s2{"how"};
-  std::string s3{"are"};
-  auto e1 = std::make_tuple(s1, s2, 5.4);
-  auto e2 = std::make_tuple(s2, s3, 7.6);
-  auto e = std::vector<std::tuple<std::string, std::string, double>>{e1, e2};
-  gdwg::Graph<std::string, double> b1{e.begin(), e.end()};
-  std::vector<std::string> lis1 = b1.GetNodes();
-  for (int i = 0; i < lis1.size(); ++i) {
-    std::cout << lis1[i] + "----";
-  }
-  std::cout << "\n";
+  // // // test for tuple iterator constructor
+  // std::string s1{"Hello"};
+  // std::string s2{"how"};
+  // std::string s3{"are"};
+  // auto e1 = std::make_tuple(s1, s2, 5.4);
+  // auto e2 = std::make_tuple(s2, s3, 7.6);
+  // auto e = std::vector<std::tuple<std::string, std::string, double>>{e1, e2};
+  // gdwg::Graph<std::string, double> b1{e.begin(), e.end()};
+  // std::vector<std::string> lis1 = b1.GetNodes();
+  // std::cout << "graph b1 is: \n";
+  // for (int i = 0; i < lis1.size(); ++i) {
+  //   std::cout << lis1[i] + " ";
+  // }
+  // std::cout << "\n";
 
-  //  std::cout<<b.value()<<'\n';
-  std::vector<std::string> a{"a", "how", "c", "you"};
-  gdwg::Graph<std::string, int> p{a.begin(), a.end()};
-  std::string u{"c"};
-  p.InsertEdge("a", u, 1);
-  std::cout<<p.IsNode("a")<<"\n";
-  p.IsConnected("a","c");
-  std::cout<<  p.IsConnected("a","c")<<"\n";
-  std::cout<<  p.IsConnected("how","c")<<"\n";
-  
-  std::vector<std::string> lis = p.GetNodes();
-  // auto how = p.IsNode("how");
-  // std::cout << how << "========";
-  
-  auto how = p.findNode("how");
-  std::cout << how->getval() << "========\n";
+  // // test for copy constructor
+  // gdwg::Graph<std::string, double> b2{b1};
+  // std::vector<std::string> lis2 = b2.GetNodes();
+  // std::cout << "graph b2 is: \n";
+  // for (int i = 0; i < lis2.size(); ++i) {
+  //   std::cout << lis2[i] + " ";
+  // }
+  // std::cout << "\n";
 
-  for (int i = 0; i < lis.size(); ++i) {
-    std::cout << lis[i] + " ";
-  }
-  std::cout << "\n";
+  // //  std::cout<<b.value()<<'\n';
+  // std::vector<std::string> a{"a", "how", "c", "you"};
+  // gdwg::Graph<std::string, int> p{a.begin(), a.end()};
+  // std::string u{"c"};
+  // std::cout << "InsertNode(a): " << p.InsertNode("a") << "\n";
+  // std::cout << "InsertNode(b): " << p.InsertNode("b") << "\n";
 
-  gdwg::Graph<char, std::string> z{'a', 'b', 'x', 'y'};
-  auto li = z.GetNodes();
-  for (int i = 0; i < li.size(); ++i) {
-    std::cout << li[i];
-  }
-  std::cout << "\n";
+  // p.InsertEdge("a", u, 1);
+  // std::cout << "IsNode(a): " << p.IsNode("a") << "\n";
+  // p.IsConnected("a", "c");
+  // std::cout << "IsConnected(a, c): " << p.IsConnected("a", "c") << "\n";
+  // std::cout << "IsConnected(how, c): " << p.IsConnected("how", "c") << "\n";
+
+  // std::vector<std::string> lis = p.GetNodes();
+  // // auto how = p.IsNode("how");
+  // // std::cout << how << "========";
+
+  // std::cout << "p.findNode(how)->getval()" << p.findNode("how")->getval()
+  //           << "\n";
+
+  // for (int i = 0; i < lis.size(); ++i) {
+  //   std::cout << lis[i] + " ";
+  // }
+  // std::cout << "\n";
+
+  // gdwg::Graph<char, std::string> z{'a', 'b', 'x', 'y'};
+  // auto li = z.GetNodes();
+  // for (int i = 0; i < li.size(); ++i) {
+  //   std::cout << li[i];
+  // }
+  // std::cout << "\n";
 }
--- assignments/dg/graph.tpp
+++ assignments/dg/graph.tpp
@@ -1,2 +1,19 @@
-
 // constructor
+template <typename N, typename E>
+gdwg::Graph<N, E>::Graph(typename std::vector<N>::const_iterator first,
+                         typename std::vector<N>::const_iterator last) {
+  for (auto &it = first; it != last; ++it) {
+    // if (!this->IsNode(*it)) {
+    Node new_node = Node{*it};
+    nodes_.insert(std::make_pair(*it, std::make_shared<Node>(new_node)));
+    // }
+  }
+}
+
+template <typename N, typename E>
+void gdwg::Graph<N, E>::printG(){
+    for (typename std::map<N, std::shared_ptr<Node>>::iterator it = nodes_.begin();
+       it != nodes_.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+}
